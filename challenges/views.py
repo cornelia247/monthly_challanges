f970d57ebe3cba6398d312dc4acedba9cf301fcb
cornelia247 <ekpenyongcornelia2@gmail.com>
2022-11-28 19:27:19 +0100

    links all work!

--- challenges/views.py
+++ challenges/views.py
@@ -3,6 +3,19 @@ from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
 
 # Create your views here.
+
+
+def index(request):
+    list_items = ""
+    months = list(monthly_challanges.keys())
+    for month in months :
+        capitalized_month = month.capitalize()
+        month_path = reverse("month-challenge", args=[month])
+        list_items += f"<li><a href=\"{month_path}\"> {capitalized_month}</a></li>"
+    response_data = f"<ul>{list_items}</ul>"
+    return HttpResponse(response_data)
+
+
 monthly_challanges = {
     'january': 'Learn Everything about Django',
     'febuary': 'Learn how to be Organised',
@@ -22,7 +35,7 @@ monthly_challanges = {
 
 def monthly_challenge_by_number(request, month):
     months = list(monthly_challanges.keys())
-    
+
     if month > len(months):
         return HttpResponseNotFound("Invalid Month")
     redirect_month = months[month - 1]
@@ -33,6 +46,7 @@ def monthly_challenge_by_number(request, month):
 def monthly_challange(request, month):
     try:
         challenge_text = monthly_challanges[month]
-        return HttpResponse(challenge_text)
+        response_data = f"<h1>{challenge_text}</h1>"
+        return HttpResponse(response_data)
     except:
-        return HttpResponseNotFound('This Month is not supported')
+        return HttpResponseNotFound('<h1>This Month is not supported!</h1>')
