ce615f9072af6277817f03377ad84addf1ea3e89
cornelia247 <ekpenyongcornelia2@gmail.com>
2022-11-28 14:58:42 +0100

    update for the day

--- challenges/views.py
+++ challenges/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def january(request):
+    return HttpResponse("Learn Everything about Django")
+
+def febuary(request):
+    return HttpResponse("Learn how to be Origanised")
