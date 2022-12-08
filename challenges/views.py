f83fa6d06675be2ba5347c119ec7bf42f0fdb37e
cornelia247 <ekpenyongcornelia2@gmail.com>
2022-12-08 18:55:53 +0100

    commit

--- challenges/views.py
+++ challenges/views.py
@@ -1,19 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.http import Http404, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
+
 
 # Create your views here.
 
 
 def index(request):
-    list_items = ""
     months = list(monthly_challanges.keys())
-    for month in months :
-        capitalized_month = month.capitalize()
-        month_path = reverse("month-challenge", args=[month])
-        list_items += f"<li><a href=\"{month_path}\"> {capitalized_month}</a></li>"
-    response_data = f"<ul>{list_items}</ul>"
-    return HttpResponse(response_data)
+    return render(request, "challenges/index.html",{
+        "months": months
+    })
+  
 
 
 monthly_challanges = {
@@ -28,7 +26,7 @@ monthly_challanges = {
     'september': 'Have a glorious birthday',
     'october': 'Practice public speaking',
     'november': ' Write two certificate exams',
-    'december': 'Have your first detty christmas'
+    'december': None
 
 }
 
@@ -46,7 +44,8 @@ def monthly_challenge_by_number(request, month):
 def monthly_challange(request, month):
     try:
         challenge_text = monthly_challanges[month]
-        response_data = f"<h1>{challenge_text}</h1>"
-        return HttpResponse(response_data)
+        return render(request,'challenges/challenge.html',{"text": challenge_text,
+        "month": month,})
+        
     except:
-        return HttpResponseNotFound('<h1>This Month is not supported!</h1>')
+        raise Http404()
